9955f2618cd693a5901e637e3801ed6fd6af34f1
Jussi Pakkanen <jpakkane@gmail.com>
2018-08-15 21:46:51 +0300

    Added test for registryentries.

--- run_tests.py
+++ run_tests.py
@@ -66,6 +66,7 @@ if __name__ == '__main__':
                 ('msvcrt', 'msvcrt.json'),
                 ('icons', 'icons.json'),
                 ('shortcuts', 'shortcuts.json'),
+                ('registryentries', 'registryentry.json')
     ]
 
     build_binaries()
